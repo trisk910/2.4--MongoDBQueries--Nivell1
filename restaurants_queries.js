a37015ad0b2b3a1378d127fe7513e776c16082a8
Marc Bernabeu Rodriguez <marc_b_r@hotmail.com>
2024-11-26 13:44:34 +0100

    13 queries realitzades

--- restaurants_queries.js
+++ restaurants_queries.js
@@ -3,50 +3,66 @@ db.restaurants.find({});
 
 // 2. Escriu una consulta per mostrar el restaurant_id, name, borough i cuisine per tots els documents en la col·lecció Restaurants.
 db.restaurants.find(
-    {},
     {restaurant_id: 1, name: 1, borough: 1, cuisine: 1, _id: 0}
 );
 
 // 3. Escriu una consulta per mostrar el restaurant_id, name, borough i cuisine, però exclou el camp _id per tots els documents en la col·lecció Restaurants.
-db.restaurants.find({},
+db.restaurants.find(
 	{restaurant_id: 1, name: 1, borough: 1, cuisine: 1}
 );
 
 // 4. Escriu una consulta per mostrar restaurant_id, name, borough i zip code, però exclou el camp _id per tots els documents en la col·lecció Restaurants.
-db.restaurants.find({},
+db.restaurants.find(
 	{restaurant_id: 1, name: 1, borough: 1, "address.zipcode": 1, _id: 0}
 );
 
 // 5. Escriu una consulta per mostrar tots els restaurants que estan en el Bronx.
-db.restaurants.find({},
+db.restaurants.find(
 	{borough: "Bronx"}
 );
 
 // 6. Escriu una consulta per mostrar els primers 5 restaurants que estan en el Bronx.
-db.restaurants.find({},
+db.restaurants.find(
 	{borough: "Bronx"}
 ).limit(5);
 
 // 7. Escriu una consulta per mostrar el pròxim 5 restaurants després de saltar els primers 5 del Bronx.
-db.restaurants.find({},
+db.restaurants.find(
 	{borough: "Bronx"}
 ).skip(5).limit(5) ;
 
 // 8. Escriu una consulta per trobar els restaurants que tenen un score de més de 90.
-db.restaurants.find({},
+db.restaurants.find(
 	{'grades.score': {$gt: 90}}
 );
 
 // 9. Escriu una consulta per trobar els restaurants que tenen un score de més de 80 però menys que 100.
-db.restaurants.find({},
+db.restaurants.find(
 	{'grades.score': {$gt: 90,$lt: 100}}
 );
 
 // 10. Escriu una consulta per trobar els restaurants que es localitzen en valor de 
 //latitud menys de -95.754168.
-  db.restaurants.find({},
-	{ 'address.coord.0': { $lt: -95.754168 } }
+  db.restaurants.find(
+	{ 'address.coord.1': { $lt: -95.754168 } }
 );
 
 // 11. Escriu una consulta de MongoDB per a trobar els restaurants que no preparen 
 // cap cuisine de 'American' i la seva qualificació és superior a 70 i longitud inferior a -65.754168.
+  db.restaurants.find(
+	{ $and:[{cuisine: { "$ne": "American" }}, {"grades.score": { "$gt": 70 }}, {"address.coord.0": { "$lt": -65.754168 }}]}
+);
+
+// 12. Escriu una consulta per trobar els restaurants que no preparen cap cuisine de 'American' 
+// i van aconseguir un marcador més de 70 i localitzat en la longitud menys que -65.754168. Nota: Fes aquesta consulta sense utilitzar $and operador.
+db.restaurants.find(
+	{cuisine: { "$ne": "American" }, "grades.score": { "$gt": 70 }, "address.coord.0": { "$lt": -65.754168 }}
+);
+
+// 13. Escriu una consulta per trobar els restaurants que no preparen cap cuisine de 'American' i van obtenir un punt de grau 'A' no pertany a Brooklyn. 
+// S'ha de mostrar el document segons la cuisine en ordre descendent.
+db.restaurants.find(
+  {cuisine: { "$ne": "American" }, "grades.grade": "A", borough: { "$ne": "Brooklyn" }}
+).sort({ cuisine: -1 });
+
+// 14. Escriu una consulta per trobar el restaurant_id, name, borough i cuisine per a aquells restaurants que contenen 'Wil' com les tres primeres lletres en el seu nom.
